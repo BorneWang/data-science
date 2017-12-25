c80d62f15270c486e7c749983f4d6f566416d189
BorneWang <bornewang@163.com>
2017-12-25 20:27:58 +0800

    Update MNIST_ANN.py

--- MNIST_ANN.py
+++ MNIST_ANN.py
@@ -1,43 +1,58 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
+MNIST analysis using Bagging-ANN
+王博文
+1410069
+信息与计算
 """
-#-----------------------------head-----------------------------------------
+
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+from sklearn.metrics import f1_score
+
 import numpy as np
-from sklearn.datasets import load_iris
-from sklearn.cross_validation import train_test_split
-import matplotlib.pyplot as plt
-iris = load_iris()
 
-#-------------------------train_test_split---------------------------------
-x_train1,x_test1,y_train1,y_test1 = train_test_split(iris.data[0:50],
-                                                     iris.target[0:50],
-                                                     test_size=0.5,
-                                                     random_state=1)
-x_train2,x_test2,y_train2,y_test2 = train_test_split(iris.data[50:100],
-                                                     iris.target[50:100],
-                                                     test_size=0.5,
-                                                     random_state=1)
-x_train3,x_test3,y_train3,y_test3 = train_test_split(iris.data[100:150],
-                                                     iris.target[100:150],
-                                                     test_size=0.5,
-                                                     random_state=1)
-x_train = np.array([x_train1,x_train2,x_train3]).reshape(75,4)
-y_train = np.array([y_train1,y_train2,y_train3]).reshape(75)
-x_test = np.array([x_test1,x_test2,x_test3]).reshape(75,4)
-y_test = np.array([y_test1,y_test2,y_test3]).reshape(75)
+X_train = mnist.train.images
+X_test = mnist.test.images
 
-#-------------------------AdaBoostClassifier--------------------------------
-from sklearn.ensemble import AdaBoostClassifier
-clf = AdaBoostClassifier(random_state=1,n_estimators=100)
-clf.fit(x_train,y_train)
+y_train = np.zeros(55000,dtype=int)
+y_test = np.zeros(10000,dtype=int)
+for i in range(55000):
+    for j in range(9):
+        if mnist.train.labels[i][j] == 1:
+            y_train[i] = j
+            
+for i in range(10000):
+    for j in range(9):
+        if mnist.test.labels[i][j] == 1:
+            y_test[i] = j
+                        
+#--------------------------NeuralNetwork----------------------------------
+from sklearn.neural_network import MLPClassifier
+ ANN = MLPClassifier(hidden_layer_sizes=(529,), solver='adam', 
+                     activation='tanh')
+ANN.fit(X_train,mnist.train.labels)
+f1_score(mnist.test.labels,ANN.predict(X_test),average='macro')
+f1_score(mnist.train.labels,ANN.predict(X_train),average='macro')  
 
-#---------------------------result------------------------------------------
-clf.predict(x_train)
-clf.score(x_train,y_train)
-clf.predict(x_test)
-clf.score(x_test,y_test)
-fi = clf.feature_importances_
-  
+ANN_f1score = np.zeros(20)
+for i in range(519,539):
+     ANN = MLPClassifier(hidden_layer_sizes=(i,), solver='adam', 
+                         activation='tanh')
+    ANN_f1score[i-519]=f1_score(mnist.test.labels,ANN.predict(X_test), 
+                               average='macro')
+
+#----------------------------BaggingClassifier-------------------------------
+from sklearn.ensemble import BaggingClassifier
+BR = BaggingClassifier(base_estimator=ANN,n_estimators=10,n_jobs=5)
+BR.fit(X_train,y_train)
+BR.score(X_test,y_test)        
+f1_score(y_test,BR.predict(X_test),average='macro')
+f1_score(y_train,BR.predict(X_train),average='macro')
+            
+#------------------------------SVC-----------------------------------------
+from sklearn.svm import SVC
+svm = SVC()
+svm.fit(X_train,y_train)
+svm.score(X_test,y_test)
