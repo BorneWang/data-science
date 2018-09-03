80fbdacb00b7b7b48316deb5b7b69c0f15e07c2c
BorneWang <bornewang@163.com>
2018-09-02 20:51:30 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Sep  1 21:23:17 2018
-
 @author: admin
 """
 
@@ -30,28 +29,7 @@ class Segment():
     def __init__(self):
         self.frameIdList = []
     def GetFrameIdList(self):
-        return self.frameIdList
-
-class UnitResult():
-    def __init__(self,frameID,x,y,w,h,confidence,label):
-        self.frameID = frameID
-        self.x = x
-        self.y = y
-        self.w = w
-        self.h = h
-        self.confidence = confidence
-        self.label = label
-        self.res
-
-class Result():
-    def __init__(self):
-        self.unitResults = []
-    def AddResult(self, other):
-        for unit in other.unitResults:
-            self.unitResults.append(unit)
-    def CountObjts(self):
-        objs = []
-        for oneresult in self.unitResult:
+        return self.frameIdList        for oneresult in self.unitResult:
             frameID = oneresult.frameID
             if frameID in objs:
                 objs[frameID] += 1
@@ -68,40 +46,22 @@ def increaseRes(oldres):
         newres = 1
     return newres
 
-            
+
 
 class Filtering():
     def __init__(self,args):
         self.logic = args.logic
-        
+
     def Run(self,segment):
         if self.logic == LOGIC_DDS:
             return segment
         else:
             return segment
- 
+
 class DecisionMaker():
     def __init__(self,args):
-        self.logic = args.logic
-        # DDS fields
-        self.maxThres = 0.8
-        self.minThres = 0.3
-        self.DiffDic = {}
-        # GroundTruth fields
-        self.var3 = 0
-        # NoScope/Glimpse/Vigil fields
-    
-    def GetNextRegionToQuery(self, segment, Results):
-        regions = []
-        frames = segment.GetFrameIdList()
-        if self.logic == LOGIC_GROUNDTRUTH:
-            for frameId in frames:
-                regions.append(Region(frameId,0,0,1,1,RES_HIGH))
-            return regions
-        if self.logic == LOGIC_DDS:
-            if Results == []:
-                regions.append(Region(frames[0],0,0,1,1,0.25))
-                regions.append(Region(frames[len(frames)-1],0,0,1,1,0.25))
+        self.logic = args.logic                regions.append(Region(frames[0],0,0,1280,720,0.25))
+                regions.append(Region(frames[len(frames)-1],0,0,1280,720,0.25))
                 self.DiffDic[(frames[0],frames[len(frames)-1])] = -1
                 return regions
             else:
@@ -114,7 +74,7 @@ class DecisionMaker():
                     self.DiffDic[key] = abs(diff)
                     if diff > maxdiff:
                         maxkey = key
-                        
+
                         #FrameSelection
                         if maxkey[1] - maxkey[0] > 1:
                             midframe = int(maxkey[1] - maxkey[0] / 2)
@@ -122,7 +82,7 @@ class DecisionMaker():
                             self.DiffDic[(maxkey[0],midframe)] = -1
                             self.DiffDic[(maxkey[1],midframe)] = -1
                             regions.append(Region(midframe,0,0,1,1,0.25))
-            
+
                         #Croping
                         for ret in Results:
                             if ret.confidence > self.minThres and ret.confidence < self.maxThres:
@@ -133,23 +93,8 @@ class DecisionMaker():
                                                               ret.y,
                                                               ret.w,
                                                               ret.h,
-                                                              newResolution))
-            return regions
-        #if self.logic == Noscope:
-        #    return None
-       # if self.logic == Vigil:
-        #    return None
-            
-            
+                                  
 
-class Client():
-    def __init__(self,args,srv):
-        self.src = args.src
-        self.logic = DecisionMaker(args)
-        self.filtering = Filtering(args)
-        self.server = srv
-       
-        
     def getImage(self,region):
         frameID = str(region.frameID)
         frameID = frameID.zfill(10)
@@ -159,64 +104,126 @@ class Client():
         h = region.h
         x = region.x
         y = region.y
-        os.system("ffmpeg -i {0} -vf scale=iw*{1}:ih*{1} -filter:v \"crop={}:{}:{}:{}\" {}".format(framePath,
+        print("ffmpeg framepath is",framePath)
+        os.system("ffmpeg -i {0} -vf scale=iw*{1}:ih*{1} -filter:v \"crop={2}:{3}:{4}:{5}\" -y {6}".format(framePath,
                                                                                                    region.res,
                                                                                                    w,
                                                                                                    h,
                                                                                                    x,
                                                                                                    y,
                                                                                                    impath))
+        print("ffmpeg impath is",impath)
         return impath
- 
-              
+
+
     def QueryCNN(self, Regions):
-        Results = []
+        Results = Result()
         for region in Regions:
             impath = self.getImage(region)
-            infresult = self.server.RUN_CNN(impath)
+            print("client impath is ",impath)
+            infresult = self.server.RUNCNN(impath)
             for line in infresult:
                 lineresult = line.split(' ')
-                x = int(lineresult[2]) 
-                y = int(lineresult[3])
-                w = int(lineresult[4]) - x
-                h = int(lineresult[5]) - y
-                conf = int(lineresult[1])
+                x = float(lineresult[2])
+                y = float(lineresult[3])
+                w = float(lineresult[4]) - x
+                h = float(lineresult[5]) - y
+                conf = float(lineresult[1])
                 label = lineresult[0]
                 uniret = UnitResult(region.frameID,x,y,w,h,conf,label,region.res)
-                Results.append(uniret)
+                Results.unitResults.append(uniret)
         return Results
-    
-    def getNextSegment(self,now,maxsize):
-        Allframes = sorted(glob.glob(self.src))
+
+    def getNextSegment(self,now,maxsize=4):
+        Allframes = sorted(glob.glob('{}/*.png'.format(self.src)))
+        print("src is ",self.src)
         seg = Segment()
         now += 1
         if now == len(Allframes):
             return seg
-        count = 0
+                                                          newResolution))
+            return regions
+        #if self.logic == Noscope:
+        #    return None
+       # if self.logic == Vigil:
+        #    return None
+
+
+
+class Client():
+    def __init__(self,args,srv):
+        self.src = args.src
+        self.logic = DecisionMaker(args)
+        self.filtering = Filtering(args)
+        self.server = srv
+
+        # DDS fields
+        self.maxThres = 0.8
+        self.minThres = 0.3
+        self.DiffDic = {}
+        # GroundTruth fields
+        self.var3 = 0
+        # NoScope/Glimpse/Vigil fields
+
+    def GetNextRegionToQuery(self, segment, Results):
+        regions = []
+        frames = segment.GetFrameIdList()
+        if self.logic == LOGIC_GROUNDTRUTH:
+            for frameId in frames:
+                regions.append(Region(frameId,0,0,1280,720,1))
+            return regions
+        if self.logic == LOGIC_DDS:
+            if Results == []:
+
+
+class UnitResult():
+    def __init__(self,frameID,x,y,w,h,confidence,label,res):
+        self.frameID = frameID
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.confidence = confidence
+        self.label = label
+        self.res = res
+
+class Result():
+    def __init__(self):
+        self.unitResults = []
+    def AddResult(self, other):
+        for unit in other.unitResults:
+            self.unitResults.append(unit)
+    def CountObjts(self):
+        objs = []
+count = 0
         for frame in Allframes[now:]:
             now +=1
-            frameID = int(frame[:len(frame)-4])
+            print("frame is ",frame[len(frame)-14:len(frame)-4])
+            frameID = int(frame[len(frame)-14:len(frame)-4])
             seg.frameIdList.append(frameID)
             count += 1
             if count == maxsize:
                 return seg, now
-        return seg,now      
-        
+        return seg,now
 
-    
-            
-    def UpdateResult(frames,Results):
-        for ret in Results:
-             if ret.frameID in frames.GetFrameIdList:
-                 frames.GetFrameIdList.pop(ret.frameID)
-    
+
+
+
+    def UpdateResult(self,frames,Results):
+        for ret in Results.unitResults:
+             print("ret.frameID is",ret.frameID)
+             if ret.frameID in frames.frameIdList:
+                 frames.frameIdList.remove(ret.frameID)
+        return frames
+
     #def Track(frames):
-    
-    
+
+
     def Run(self):
         now = -1
         while True:
             segment,now = self.getNextSegment(now)
+            print("segment is",segment.frameIdList[0])
             if segment.GetFrameIdList == []:
                 return None
             FramdIDAfterLoacalFiltering = self.filtering.Run(segment)
@@ -225,11 +232,8 @@ class Client():
             while True:
                 RegionsToQuery = self.logic.GetNextRegionToQuery(FramdIDAfterLoacalFiltering,
                                                                 results)
-                if RegionsToQuery == []:
+                if len(RegionsToQuery) == 0:
                     break
                 CNNResult = self.QueryCNN(RegionsToQuery)
                 results.AddResult(CNNResult)
-                FramdIDAfterLoacalFiltering = self.UpdateResult(FramdIDAfterLoacalFiltering,
-                                                                results)
-            #if FramdIDAfterLoacalFiltering != []:
-                #Track(FramdIDAfterLoacalFiltering)
+                FramdIDAfterLoacalFiltering = self.UpdateResult(FramdIDAfterLoacalFiltering,results)
