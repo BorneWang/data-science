69602d5798adede1dfcb56ba9de3bf0393e5238b
BorneWang <bornewang@163.com>
2018-09-02 20:46:07 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,41 +1,29 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Sep  1 21:23:07 2018
-
 @author: admin
 """
 
 
-import argparse
-import ast
-import pprint
 import cv2
 import os
-import np
+import numpy as np
+import time
 
-import mxnet as mx
-from mxnet.module import Module
-
-from symdata.bbox import im_detect
-from symdata.loader import load_test, generate_batch
-from symdata.vis import vis_detection
-from symnet.model import load_param, check_shape
-from model import ModelRUN
-from client import UnitResult
-
-class server():
-        
+class Server():
     def RUNCNN(self,impath):
+        print("impath is",impath)
         im = cv2.imread(impath)
         im_shape = im.shape
         im_size_min = np.min(im_shape[0:2])
         im_size_max = np.max(im_shape[0:2])
         outfile = impath + 'serverresult'
-        os.system("python3 demo.py --dataset voc --network resnet101 --params resnet101_voc0010.params --img-short-side {} --img-long-side {} --image {} --out {}".format(im_size_min,im_size_max,impath,outfile))
+        tic = time.time()
+        os.system("python3 /home/bowen/python-DDS/rcnn/demo2.py --gpu 0 --dataset voc --network resnet101 --params rcnn/resnet_voc0712-0010.params --img-short-side {} --img-long-side {} --image {} --out {}".format(im_size_min,im_size_max,impath,outfile))
+        tic2 = time.time()
+        print("Run Cnn time is ",tic2-tic)
         CNN_result = []
         with open(outfile) as f:
             for line in f:
-                CNN_result.append(line) 
-        os.system("rm {}".format(outfile))
+                CNN_result.append(line)
         return CNN_result
-        
