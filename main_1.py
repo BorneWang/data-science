ff7398f571e7b95f8e36752dbe0af9661e421239
BorneWang <bornewang@163.com>
2018-09-04 00:34:41 -0500

    Update main_1.py

--- main_1.py
+++ main_1.py
@@ -5,8 +5,8 @@ Created on Sat Sep  1 21:22:42 2018
 """
 
 import argparse
-from server_1 import Server
-from client import Client
+from server_2 import Server
+from client_3 import Client
 import time
 
 def main():
@@ -17,15 +17,15 @@ def main():
     parser.add_argument('--logic', type=str, help='logic')
     parser.add_argument('--params', type=str, default='resnet_voc0712-0010.params', help='CNN model')
     srcargs = parser.parse_args()
-
+   
     Srv = Server(srcargs)
     print("creat server success")
     Clt = Client(srcargs,Srv)
-
+    
     Clt.Run()
     tic_end = time.time()
     print("the whold run time is", tic_end-tic0)
-
-
+    
+    
 if __name__ == '__main__':
     main()
